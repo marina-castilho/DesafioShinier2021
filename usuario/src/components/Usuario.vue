c21c3caf0dfca2929c811459773e2a87c615cf97
marina-castilho <marina.castilho@outlook.com>
2021-04-08 17:35:11 -0300

    Tentativa falha .vue
    Não consegui seguir com esse novo projeto, vou tentar o anterior novamente

---
+++ usuario/src/components/Usuario.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="cabecalho">
+    <img src="assets/logo_an 1.png" alt="Analitcs" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
